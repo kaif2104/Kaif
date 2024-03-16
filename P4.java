f1c5b9e0c9a292a7e1c1970b08c48529b3d9e816
kaif2104 <163647721+kaif2104@users.noreply.github.com>
2024-03-16 17:18:53 +0530

    Update P4.java

--- P4.java
+++ P4.java
@@ -3,7 +3,7 @@ public class P4 {
     public static void main(String[] args) {
         int radius;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the radius: ");
+        System.out.print("Enter the Radius: ");
         radius = sc.nextInt();
         double circumference= Math.PI * 2*radius;
     System.out.printf( "\nCircumference is: %.2f",circumference) ;
