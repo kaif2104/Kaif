ad128edde0cb31e4235c020fc17fe62fd50d281f
kaif2104 <163647721+kaif2104@users.noreply.github.com>
2024-03-16 17:17:42 +0530

    Add files via upload

---
+++ P4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class P4 {
+    public static void main(String[] args) {
+        int radius;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the radius: ");
+        radius = sc.nextInt();
+        double circumference= Math.PI * 2*radius;
+    System.out.printf( "\nCircumference is: %.2f",circumference) ;
+        
+    }
+    
+}
